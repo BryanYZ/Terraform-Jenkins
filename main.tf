9aa0306793e27628a3a65138318d962ee751d1e6
BryanYZ <50778021+BryanYZ@users.noreply.github.com>
2024-03-23 02:55:39 -0400

    main

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "foo" {
   ami           = "ami-0b8b44ec9a8f90422" # us-east-2, 1vCPU 1GB RAM
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "TF-Instance01"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0b8b44ec9a8f90422" # us-east-2, 1vCPU 1GB RAM
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance02"
   }
 }
